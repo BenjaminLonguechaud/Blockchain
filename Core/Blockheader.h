337ce8d5ae31cf99c68992d53c1058831957c92b
Benjamin L <benjamin.longuechaud@gmail.com>
2025-12-11 15:58:27 -0800

    Add default constructor to BlockHeader and replace tabs with spaces

--- Core/Blockheader.h
+++ Core/Blockheader.h
@@ -22,25 +22,38 @@
 class BlockHeader : public CoreObject {
 public:
 
-	// Block version number
-	uint64_t version;
-	// 256-bit hash of the previous block header, forming the chain.
-	std::string hashPrevBlock;
-	// 256-bit hash based on all of the transactions in the block
-	std::string hashMerkleRoot;
-	// Current block timestamp as seconds since 1970-01-01T00:00 UTC
-	uint64_t timestamp;
-	// 32-bit number (starts at 0 and is incremented for each hash) used in the proof-of-work algorithm
-	uint32_t nonce;
-	// Difficulty is a measure of how difficult it is to find a hash below a given target.
-	uint32_t difficulty;
-	//The hash of the header itself, serving as the block's unique identifier and proof of work.
-	std::string blockHash;
+    // Block version number
+    uint64_t version;
+    // 256-bit hash of the previous block header, forming the chain.
+    std::string hashPrevBlock;
+    // 256-bit hash based on all of the transactions in the block
+    std::string hashMerkleRoot;
+    // Current block timestamp as seconds since 1970-01-01T00:00 UTC
+    uint64_t timestamp;
+    // 32-bit number (starts at 0 and is incremented for each hash) used in the proof-of-work algorithm
+    uint32_t nonce;
+    // Difficulty is a measure of how difficult it is to find a hash below a given target.
+    uint32_t difficulty;
+    //The hash of the header itself, serving as the block's unique identifier and proof of work.
+    std::string blockHash;
 
-	// Constructor to initialize BlockHeader with default values
-	BlockHeader();
+    // Constructor to initialize BlockHeader with default values
+    BlockHeader();
+    BlockHeader(uint64_t ver,
+                    const std::string& prevHash,
+                    const std::string& merkleRoot,
+                    uint64_t time,
+					uint32_t nonce,
+                    uint32_t diff)
+	    : version(ver),
+          hashPrevBlock(prevHash),
+          hashMerkleRoot(merkleRoot),
+          timestamp(time),
+          nonce(nonce),
+          difficulty(diff),
+          blockHash("") {}
 
-	// Serialize the transaction into a deterministic string
+    // Serialize the transaction into a deterministic string
     std::string serialize() const override;
 
 };
